38dde85df28e55c7db1c0880ae0ab141fd98535b
iamksatheesh <54324980+iamksatheesh@users.noreply.github.com>
2021-06-18 17:28:48 +0530

    Update UserResource.java
    New

--- src/main/java/com/mt/springmongo/UserResource.java
+++ src/main/java/com/mt/springmongo/UserResource.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 /**
  * Rest controller which is responsible for retrieving {@link User}s.
  *
- * @author Mithun
+ * @author Satheeshkumar
  */
 @RestController
 @RequestMapping(value = "/api/users")
